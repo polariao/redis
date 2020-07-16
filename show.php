570555567ef8fe7c83f27d91d63fefdb8e24749d
hja2017 <50760078+hja2017@users.noreply.github.com>
2020-07-16 13:46:40 +0800

    Update show.php

--- show.php
+++ show.php
@@ -3,6 +3,6 @@ include "connection.php";
 
 $post = $_POST;
 
-echo $connect->hget($post['no'], 'no');   //学号 
-echo $connect->hget($post['no'], 'name');   //姓名
-echo $connect->hget($post['no'], 'age');   //年龄
+echo $redis->hget($post['no'], 'no');   //学号 
+echo $redis->hget($post['no'], 'name');   //姓名
+echo $redis->hget($post['no'], 'age');   //年龄
